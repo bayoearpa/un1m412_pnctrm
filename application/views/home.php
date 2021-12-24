23c7bf41c711aac91f03e2ef421f09f7e34675d5
DESKTOP-IQRPE91\BAYU <bayu_wenger@ymail.com>
2021-12-24 14:09:43 +0700

    no message

--- application/views/home.php
+++ application/views/home.php
@@ -259,7 +259,7 @@
           </div>
         </div>
         <div class="text-center mt-4">
-         <center><img src="<?php echo base_url() ?>assets/front1/img/alur2022.png" width="100%"></center>
+         <center><img src="<?php echo base_url() ?>assets/front1/img/alur2022rev.png" width="100%"></center>
         </div>
       <!--   <div class="text-center mt-4">
          <center><img src="<?php //echo base_url() ?>assets/front1/img/alur.jpg" width="100%"></center>
