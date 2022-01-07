2e13311ff2036de8b9e04f6dfbec3a7bfd7ed8df
DESKTOP-IQRPE91\BAYU <bayu_wenger@ymail.com>
2022-01-07 15:55:27 +0700

    07012022

--- application/controllers/Bau.php
+++ application/controllers/Bau.php
@@ -117,6 +117,15 @@ class Bau extends CI_Controller {
         $this->load->view('bau/footer');
 
     }
+    public function get_summary_prov_detail_2021($id)
+    {
+        # code...
+        $where = array('id_provinsi' => $id);
+        $data['stat'] = $this->m_registrasi->get_data_join_where_2021($where)->result();
+        $this->load->view('bau/header');
+        $this->load->view('bau/summary_2021_detail_prov',$data);
+        $this->load->view('bau/footer');
+    }
     
    
 }
---
+++ application/views/bau/summary_2021_detail_prov.php
@@ -0,0 +1,57 @@
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Data Pencatarma</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>No. Pendf.</th>
+                  <th>Nama</th>
+                  <th>Tempat Lahir</th>
+                  <th>Tanggal Lahir</th>
+                  <th>Nama Ibu</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php 
+                $no = 1;
+                foreach($stat as $c){ 
+                 ?>
+                <tr>
+                  <td><?php echo $no++; ?></td>
+                  <td><?php echo $c->no ?></td>
+                  <td><?php echo $c->nama ?></td>
+                  <td><?php echo $c->tl ?></td>
+                  <td><?php echo $c->tgl_l ?></td>
+                  <td><?php echo $c->nama_i?></td>
+                </tr>
+               <?php } ?>
+                </tbody>
+                <tfoot>
+                <tr>
+                 <th>No.</th>
+                 <th>No. Pendf.</th>
+                  <th>Nama</th>
+                  <th>Tempat Lahir</th>
+                  <th>Tanggal Lahir</th>
+                  <th>Nama Ibu</th>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
