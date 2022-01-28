7d7590780afb5f14bb8babd06f14fc2044ea7602
DESKTOP-IQRPE91\BAYU <bayu_wenger@ymail.com>
2022-01-28 13:16:30 +0700

    28012022

--- application/views/registrasi.php
+++ application/views/registrasi.php
@@ -292,8 +292,8 @@
                   <select class="form-control" name="gelombang" id="gelombang">
                     <option> </option>
                     <option value="1">Januari</option>
-                    <!-- <option value="2">Februari</option>
-                    <option value="3">Maret</option>
+                    <option value="2">Februari</option>
+                    <!-- <option value="3">Maret</option>
                     <option value="4">April</option>
                     <option value="5">Mei</option>
                     <option value="6">Juni</option>
