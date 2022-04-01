c26972ccc4eba967de68138f4043a8069d22c499
DESKTOP-IQRPE91\BAYU <bayu_wenger@ymail.com>
2022-04-01 15:29:34 +0700

    01042022

--- application/views/home.php
+++ application/views/home.php
@@ -75,7 +75,7 @@
           <div class="col-lg-16 ml-auto">
             <div class="text-center mt-4">
          <!-- <center><img src="<?php //echo base_url() ?>assets/front1/img/poster2.jpeg" width="100%"></center> -->
-         <center><img src="<?php echo base_url() ?>assets/front1/img/banner2022mar.jpeg" width="100%"></center>
+         <center><img src="<?php echo base_url() ?>assets/front1/img/banner2022april.jpeg" width="100%"></center>
           </div>
             <p class="lead">UNIMAR AMNI Semarang adalah sebuah Perguruan Tinggi yang diselenggarakan oleh masyrakat yang mengembangkan studi di bidang maritim dan transpor. Awalnya bernama “AMNI” ( Akademi Maritim Nasional Indonesia ) yang didirikan pada tanggal 9 September 1963. Sejalan dengan perkembangan kebutuhan dibidang  maritim dan transpor, maka pada tanggal 17 April 2007 , AMNI mengembangkan diri menjadi  Sekolah  Tinggi Maritim dan Transpor “ AMNI “ ( UNIMAR “ AMNI “) Semarang, sesuai dengan SK Mendiknas No. 53/D/O/2007.</p>
              <p class="lead">UNIMAR AMNI Semarang bersertifikat ISO 9001:2015, semua Program Studi Terakreditasi Badan Akreditasi Nasional Perguruan Tinggi ( BAN PT ) Direktorat Jendral Pendidikan Tinggi dan Approval Direktorat Jendral Perhubungan Laut / Internasional Maritime Organization ( IMO ), dengan Program Studi:</p>
@@ -139,6 +139,13 @@
           <a class="btn btn-xl btn-outline-warning js-scroll-trigger" href="#portfolio">
             <i class="fa fa-sign-in mr-2"></i>
             <font color="black">Daftar Sekarang!</font>
+          </a>
+        </div>
+         <div class="text-center mt-4">
+           <center><h1 class="text-uppercase"> PENGUMUMAN KELULUSAN PERIODE MARET</h1></center>
+          <a class="btn btn-xl btn-outline-warning js-scroll-trigger" href="https://unimar-amni.ac.id/new/2022/04/01/pengumuman-calon-mahasiswa-taruna-universitas-maritim-amni-semarang-yang-dinyatakan-lulus-dan-diterima-periode-januari-maret-ta-2022-2023/" target="__blank">
+            <i class="fa fa-sign-in mr-2"></i>
+            <font color="black">Cek Disini</font>
           </a>
         </div>
       </div>
