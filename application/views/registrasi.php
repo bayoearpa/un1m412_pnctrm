534424177cf8e15438006704546c060f33d209d3
DESKTOP-IQRPE91\BAYU <bayu_wenger@ymail.com>
2022-04-27 09:44:36 +0700

    27042022

--- application/views/registrasi.php
+++ application/views/registrasi.php
@@ -295,9 +295,9 @@
                     <!-- <option value="1">Januari</option> -->
                     <!-- <option value="2">Februari</option> -->
                     <!-- <option value="3">Maret</option> -->
-                    <option value="4">April</option>
-                   <!--  <option value="5">Mei</option>
-                    <option value="6">Juni</option>
+                    <!-- <option value="4">April</option> -->
+                    <option value="5">Mei</option>
+                  <!--   <option value="6">Juni</option>
                     <option value="7">Juli</option>
                     <option value="8">Agustus</option>
                     <option value="9">September</option>
