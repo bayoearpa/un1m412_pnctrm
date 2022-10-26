c5fd89e9698df3eec40aafbb1ba58cf684f877ac
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-10-26 19:35:44 +0700

    26102022

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'welcome';
 //registrasi route
+$route['default_controller'] = 'error_ctr';
 //ditutup karena sudah berakhir
 $route['registrasi'] = 'welcome/registrasi';
 $route['registrasi_d3'] = 'welcome/registrasid3';
