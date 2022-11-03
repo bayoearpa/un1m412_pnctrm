d2a6a3317fca51f893a7d488ec2ac1db86b08d6e
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-11-03 17:25:59 +0700

    03112022

--- application/views/home.php
+++ application/views/home.php
@@ -165,7 +165,7 @@
                   <p class="lead">Klik Untuk Lihat Detail</p>
                 </div>
               </div>
-              <img class="img-fluid" src="<?php echo base_url() ?>assets/front1/img/portfolio/P1011572.JPG" alt="">
+              <img class="img-fluid" src="<?php echo base_url() ?>assets/front1/img/portfolio/kpn23.jpg" alt="">
               <p class="lead text-center"> <font color="black">D3 KETATALAKSANAAN PELAYARAN NIAGA DAN KEPELABUHAN</font></p>
             </a>
           </div>
