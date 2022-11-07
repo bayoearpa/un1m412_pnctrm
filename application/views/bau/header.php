c0240dd91c682e6e859ccabdca0671e9bf27e2c9
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-11-07 20:22:53 +0700

    07112022

--- application/views/bau/header.php
+++ application/views/bau/header.php
@@ -137,6 +137,20 @@
             <li><a href="<?php echo base_url() ?>bau/rekap_validasi"><i class="fa fa-paperclip"></i>Rekap Validasi</a></li>
           </ul>
         </li>
+          <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Summary 2023</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="<?php echo base_url() ?>bau/get_summary_prov_2023"><i class="fa fa-money"></i>Statistik per Provinsi</a></li>
+            <li class="active"><a href="<?php echo base_url() ?>bau/get_summary_kabkota_2023"><i class="fa fa-money"></i>Statistik per Kab / Kota</a></li>
+            <li><a href="<?php echo base_url() ?>bau/get_summary_sekolah_2023"><i class="fa fa-paperclip"></i>Statistik per sekolah</a></li>
+            <li><a href="<?php echo base_url() ?>bau/get_summary_sumber_2023"><i class="fa fa-paperclip"></i>Statistik Sumber Informasi</a></li>
+          </ul>
+        </li>
 
          <li class="treeview">
           <a href="#">
