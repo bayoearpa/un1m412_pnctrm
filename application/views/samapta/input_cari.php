aeb5f922642daed9b77223643212cd8be924602d
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-11-09 11:05:38 +0700

    09112022

--- application/views/samapta/input_cari.php
+++ application/views/samapta/input_cari.php
@@ -18,7 +18,33 @@
               }//end cek data catar
               elseif ($cek_samapta == null) {
                       # code... ?>
-                      form input nilai samapta disini
+                      <!-- form input nilai samapta disini -->
+                <form method="post" action="<?php echo base_url() ?>samapta/input_carip">
+                  <div class="form-group">
+                  <label>Lari</label>
+                  <input type="number" min="1" max="100" class="form-control" id="lari" name="lari" placeholder="isi nilai dari 10-100">
+                  </div>
+
+                  <div class="form-group">
+                  <label>Push Up</label>
+                  <input type="number" min="1" max="100" class="form-control" id="push_up" name="push_up" placeholder="isi nilai dari 10-100">
+                  </div>
+
+                  <div class="form-group">
+                  <label>Pull Up</label>
+                  <input type="number" min="1" max="100" class="form-control" id="pull_up" name="pull_up" placeholder="isi nilai dari 10-100">
+                  </div>
+
+                  <div class="form-group">
+                  <label>Suttle Run</label>
+                  <input type="number" min="1" max="100" class="form-control" id="suttle_run" name="suttle_run" placeholder="isi nilai dari 10-100">
+                  </div>
+
+                  <div class="form-group">
+                  <input type="submit" class="btn btn-block btn-success" id="cek" name="cek" style="width:20%;" value="Input">
+                  </div>
+                </form>
+                      
                   
               <?php    
                     }else{ ?>
