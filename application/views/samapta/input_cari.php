7c4d7a005878a4266895fb4c8a2580417881c493
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-11-09 10:46:00 +0700

    09112022

--- application/views/samapta/input_cari.php
+++ application/views/samapta/input_cari.php
@@ -11,22 +11,13 @@
              <!-- content -->
              <?php 
              // cek data
-              foreach ($cek_validasi as $key) {
-                # code...
-                $cek_no = $key->no;
-              
-              if ($cek_no == null) {
+              if ($cek_validasi == null) {
                 # code... ?>
                 <div class=callout callout-danger><h4>Data tidak ditemukan!</h4><p>pastikan calon mahasiswa sudah melakukan registrasi.</p></div>
               <?php      
               }else{
                 # code...
-              foreach ($cek_samapta as $key) {
-                # code...
-                $cek_samapta_data = $key->no; 
-                
-
-              if ($cek_samapta_data == null) {
+              if ($cek_samapta == null) {
                       # code... ?>
                   <div class=callout callout-danger><h4>Data Sudah pernah diinput!</h4><p>pastikan calon mahasiswa belum melakukan test samapta.</p></div>
 
@@ -36,12 +27,8 @@
                # code...
               echo $key->nama;
               }
-
-
                     } ///end of cek data samapta 
-              }///end of cek samapta value data   
-              }///end of cek data pendaftar
-              }///end of cek no pendaftaran value data  
+              }///end of cek data pendaftar 
               // ./cek data
              ?>
 
