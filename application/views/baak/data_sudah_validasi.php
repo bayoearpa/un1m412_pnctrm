7d316acd032fa8b87298f5fd55171d1df1d1262c
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-11-25 14:26:19 +0700

    25112022

--- application/views/baak/data_sudah_validasi.php
+++ application/views/baak/data_sudah_validasi.php
@@ -5,7 +5,7 @@
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Data Pencatarma</h3>
+              <h3 class="box-title">Data Pencatarma Sudah Validasi</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
