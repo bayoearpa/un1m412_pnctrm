726e5c0b6a9637124d514dda77f831f57be986f3
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-12-07 16:07:30 +0700

    07122022

--- application/controllers/Baak.php
+++ application/controllers/Baak.php
@@ -397,14 +397,15 @@ class Baak extends CI_Controller {
             $data['kelas'] = $kelas;
             $data['prodi'] = $nameprodi;
              //pdf
-            // $pdfFilePath="daftar_catar_".$nameprodi.".pdf";
-            // $html=$this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data, TRUE);
-            // $pdf = $this->m_pdf->load();
+            $pdfFilePath="daftar_catar_".$nameprodi.".pdf";
+            $html=$this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data, TRUE);
+            $pdf = $this->m_pdf->load();
      
-            // $pdf->AddPage('P');
-            // $pdf->WriteHTML($html);
-            // $pdf->Output($pdfFilePath, "D");
-             $this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data);
+            $pdf->AddPage('P');
+            $pdf->WriteHTML($html);
+            $pdf->Output($pdfFilePath, "D");
+            
+             // $this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data);
         }
 
     }  
