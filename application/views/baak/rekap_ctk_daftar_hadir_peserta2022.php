df8d141a30427b1f13f37c5969bb442701ab7254
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-12-07 18:36:14 +0700

    07122022

--- application/views/baak/rekap_ctk_daftar_hadir_peserta2022.php
+++ application/views/baak/rekap_ctk_daftar_hadir_peserta2022.php
@@ -80,7 +80,7 @@ $value=array_chunk($catar, '20');
 $urut="1";
  foreach ($val as $v){ ?>
 	<tr>
-		<td height="60px" width="15px"><?php echo $urut++; ?></td>
+		<td height="40px" width="15px"><?php echo $urut++; ?></td>
 		<td><?php $text1= strtolower($v->nama);echo ucwords($text1); ?></td>
 		<td align="center" width="30px"><?php echo $v->no; ?></td>
 		<td width="15px" align="center"><?php if ($v->jk == "Pria") {
