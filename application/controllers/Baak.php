ab35b43272f19488ac5906cac3576491a82460c3
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-12-07 18:18:33 +0700

    07122022

--- application/controllers/Baak.php
+++ application/controllers/Baak.php
@@ -366,9 +366,7 @@ class Baak extends CI_Controller {
 
         $nameprodi = $this->prodi($prodi);
 
-        if ($gelombang == null) {
-            # code...
-            $where = array(
+         $where = array(
             'tbl_catar_2023.prodi' => $prodi,
             'tbl_catar_2023.kelas' => $kelas,
             'tbl_catar_2023.jk' => $jk,
@@ -377,39 +375,53 @@ class Baak extends CI_Controller {
             $data['catar'] = $this->m_registrasi->get_data_join_where($where)->result();
             $data['kelas'] = $kelas;
             $data['prodi'] = $nameprodi;
-            //  //pdf
-            // $pdfFilePath="daftar_hadir_catar_".$nameprodi.".pdf";
-            // $html=$this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data, TRUE);
-            // $pdf = $this->m_pdf->load();
-     
-            // $pdf->AddPage('P');
-            // $pdf->WriteHTML($html);
-            // $pdf->Output($pdfFilePath, "D");
-            // exit();
 
             $this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data);
-        }else{
-            $where = array(
-            'tbl_catar_2023.prodi' => $prodi,
-            'tbl_catar_2023.kelas' => $kelas,
-            'tbl_catar_2023.jk' => $jk,
-            'tbl_catar_2023.id_tgl_seleksi' => $tgl_pel,    
-            'tbl_catar_2023.gelombang' => $gelombang,                
-            );
-            $data['catar'] = $this->m_registrasi->get_data_join_where($where)->result();
-            $data['kelas'] = $kelas;
-            $data['prodi'] = $nameprodi;
-             //pdf
-            // $pdfFilePath="daftar_hadir_catar_".$nameprodi.".pdf";
-            // $html=$this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data, TRUE);
-            // $pdf = $this->m_pdf->load();
+
+        // if ($gelombang == null) {
+        //     # code...
+        //     $where = array(
+        //     'tbl_catar_2023.prodi' => $prodi,
+        //     'tbl_catar_2023.kelas' => $kelas,
+        //     'tbl_catar_2023.jk' => $jk,
+        //     'tbl_catar_2023.id_tgl_seleksi' => $tgl_pel,                
+        //     );
+        //     $data['catar'] = $this->m_registrasi->get_data_join_where($where)->result();
+        //     $data['kelas'] = $kelas;
+        //     $data['prodi'] = $nameprodi;
+        //     //  //pdf
+        //     // $pdfFilePath="daftar_hadir_catar_".$nameprodi.".pdf";
+        //     // $html=$this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data, TRUE);
+        //     // $pdf = $this->m_pdf->load();
      
-            // $pdf->AddPage('P');
-            // $pdf->WriteHTML($html);
-            // $pdf->Output($pdfFilePath, "D");
-            // exit();
-             $this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data);
-        }
+        //     // $pdf->AddPage('P');
+        //     // $pdf->WriteHTML($html);
+        //     // $pdf->Output($pdfFilePath, "D");
+        //     // exit();
+
+        //     $this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data);
+        // }else{
+        //     $where = array(
+        //     'tbl_catar_2023.prodi' => $prodi,
+        //     'tbl_catar_2023.kelas' => $kelas,
+        //     'tbl_catar_2023.jk' => $jk,
+        //     'tbl_catar_2023.id_tgl_seleksi' => $tgl_pel,    
+        //     'tbl_catar_2023.gelombang' => $gelombang,                
+        //     );
+        //     $data['catar'] = $this->m_registrasi->get_data_join_where($where)->result();
+        //     $data['kelas'] = $kelas;
+        //     $data['prodi'] = $nameprodi;
+        //      //pdf
+        //     // $pdfFilePath="daftar_hadir_catar_".$nameprodi.".pdf";
+        //     // $html=$this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data, TRUE);
+        //     // $pdf = $this->m_pdf->load();
+     
+        //     // $pdf->AddPage('P');
+        //     // $pdf->WriteHTML($html);
+        //     // $pdf->Output($pdfFilePath, "D");
+        //     // exit();
+        //      $this->load->view('baak/rekap_ctk_daftar_hadir_peserta2022',$data);
+        // }
 
     }  
     function rekap_pdf_daftarpeserta(){
