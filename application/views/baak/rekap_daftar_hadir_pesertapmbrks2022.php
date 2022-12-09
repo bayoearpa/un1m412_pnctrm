f445d1a27cc0b0c6f5b214901efb19c9aa32ad6d
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2022-12-10 02:34:48 +0700

    10122022

--- application/views/baak/rekap_daftar_hadir_pesertapmbrks2022.php
+++ application/views/baak/rekap_daftar_hadir_pesertapmbrks2022.php
@@ -10,7 +10,7 @@
                 
                   <div class="box-body">
                  <div class="form-group">
-                      <form method="post" action="<?php echo base_url() ?>baak/rekap_pdf_daftarhadirpesertadp2022">
+                      <form method="post" action="<?php echo base_url() ?>baak/rekap_pdf_daftarhadirpesertapmbrks2022">
                       <label> Pilih Prodi
                     </label>*wajib pilih
                     <select name="prodi" id="prodi" class="form-control" style="width:50%;" required="harus dipilih">  
