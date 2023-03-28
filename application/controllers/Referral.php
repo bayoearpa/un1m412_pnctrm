5d53f999fc6901e29f98fee127b0c5f12979cd2d
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-03-28 18:03:20 +0700

    28032023

--- application/controllers/Referral.php
+++ application/controllers/Referral.php
@@ -36,7 +36,7 @@ class Referral extends CI_Controller {
         	);
 
 	        // Check if user exists and password is correct
-	        $user = $this->m_registrasi->get_data($where)->row_array();
+	        $user = $this->m_registrasi->get_data($where,'tbl_ref')->row_array();
 	        if ($user && password_verify($password, $user['password'])) {
 	            // User exists and password is correct, set user session data and redirect to home page
 	            $user_data = array(
