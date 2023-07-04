5433d2003a3201ea64ac86895436cec14ef96b14
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-07-04 08:47:06 +0700

    04072023

--- application/views/bau/validasi.php
+++ application/views/bau/validasi.php
@@ -125,7 +125,7 @@
               
               <tr>
                 <th>Total Bayar:</th>
-                <td>Rp. 400.000 ,-</td>
+                <td>Rp. 500.000 ,-</td>
               </tr>
             </table>
           </div>
@@ -143,7 +143,7 @@
         <input type="hidden" name="gelombang" id="gelombang" value="<?php echo $c->gelombang ?>">
         <input type="hidden" name="tgl_byr" id="tgl_byr" value="<?php echo date("Y-m-d"); ?>">
         <input type="hidden" name="prodi" id="prodi" value="<?php echo $c->prodi ?>">
-        <input type="hidden" name="jml_byr" id="jml_byr" value="450000">
+        <input type="hidden" name="jml_byr" id="jml_byr" value="500000">
         <input type="hidden" name="thn_pel" id="thn_pel" value="<?php echo $c->thn_pel ?>">
 
 
