8efcda69220a6de3d0d424712eb10b762aa735f9
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-08-15 18:40:06 +0700

    15082023

--- application/views/cekstatusp.php
+++ application/views/cekstatusp.php
@@ -159,10 +159,11 @@
                 <div class="form-group" align="center">
                   <label for="exampleInputEmail1">Maaf anda belum melakukan registrasi atau belum melakukan validasi. lakukan validasi melalui link berikut <a href="https://linktr.ee/catarma2023"><button type="button" class="btn btn-block btn-primary btn-sm">Konfirmasi Pendaftaran</button></a>.jika belum sama sekali melakukan registrasi klik link berikut <a href="http://pencatarma.unimar-amni.ac.id/registrasi">disini</a>.</label>
                 </div> 
+                <?php foreach($catar as $c){  ?>
                 <div class="form-group" align="center">
                   <a href="<?php echo base_url() ?>download/<?php echo $c->no ?>"><button type="button" name="submit" class="btn btn-primary">Download</button></a>
                 </div>  
-                
+                <?php } ?>
                 <!-- <button type="submit" name="submit" class="btn btn-primary">Cek Data</button> -->
           </div>
           <?php } ?>
