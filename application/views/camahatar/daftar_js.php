381fea080a8fcc8d0cfd985f674414f5d92fcac4
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-11-07 03:22:35 +0700

    07112023

--- application/views/camahatar/daftar_js.php
+++ application/views/camahatar/daftar_js.php
@@ -1,43 +1,5 @@
 <script>
-  // Fungsi untuk memeriksa apakah kedua password cocok
-  function checkPasswordMatch() {
-    var password = document.getElementById("password").value;
-    var repassword = document.getElementById("repassword").value;
-    var message = document.getElementById("passwordMatchMessage");
-
-    if (password === repassword) {
-      message.innerHTML = "Password cocok!";
-      message.style.color = "green";
-    } else {
-      message.innerHTML = "Password tidak cocok.";
-      message.style.color = "red";
-    }
-  }
-
-  // Menambahkan event listener untuk memeriksa kecocokan password saat pengguna memasukkan data
-  document.getElementById("password").addEventListener("input", checkPasswordMatch);
-  document.getElementById("repassword").addEventListener("input", checkPasswordMatch);
-
-   // Fungsi untuk memeriksa panjang kata sandi
-  function checkPasswordLength() {
-    var password = document.getElementById("password").value;
-    var repassword = document.getElementById("repassword").value;
-    var passwordLengthMessage = document.getElementById("passwordLengthMessage");
-
-    if (password.length >= 8) {
-      passwordLengthMessage.innerHTML = "Panjang password mencukupi.";
-      passwordLengthMessage.style.color = "green";
-    } else {
-      passwordLengthMessage.innerHTML = "Password harus memiliki setidaknya 8 karakter.";
-      passwordLengthMessage.style.color = "red";
-    }
-  }
-
-  // Menambahkan event listener untuk memeriksa panjang password saat pengguna memasukkan data
-  document.getElementById("password").addEventListener("input", checkPasswordLength);
-  document.getElementById("repassword").addEventListener("input", checkPasswordLength);
-
-  // Fungsi untuk memeriksa panjang username
+ // Fungsi untuk memeriksa panjang username
   function checkUsernameLength() {
     var username = document.getElementById("username").value;
     var usernameLengthMessage = document.getElementById("usernameLengthMessage");
@@ -45,12 +7,52 @@
     if (username.length >= 8) {
       usernameLengthMessage.innerHTML = "Panjang username mencukupi.";
       usernameLengthMessage.style.color = "green";
+      validateForm();
     } else {
       usernameLengthMessage.innerHTML = "Username harus memiliki setidaknya 8 karakter.";
       usernameLengthMessage.style.color = "red";
+      disableForm();
     }
+  }
+
+  // Fungsi untuk memeriksa panjang password dan kesesuaian kedua password
+  function checkPassword() {
+    var password = document.getElementById("password").value;
+    var repassword = document.getElementById("repassword").value;
+    var passwordLengthMessage = document.getElementById("passwordLengthMessage");
+    var passwordMatchMessage = document.getElementById("passwordMatchMessage");
+
+    if (password.length >= 8 && repassword.length >= 8 && password === repassword) {
+      passwordLengthMessage.innerHTML = "Password mencukupi dan cocok.";
+      passwordLengthMessage.style.color = "green";
+      passwordMatchMessage.innerHTML = "Password cocok.";
+      passwordMatchMessage.style.color = "green";
+      validateForm();
+    } else {
+      passwordLengthMessage.innerHTML = "Password harus memiliki setidaknya 8 karakter.";
+      passwordLengthMessage.style.color = "red";
+      passwordMatchMessage.innerHTML = "Password tidak cocok.";
+      passwordMatchMessage.style.color = "red";
+      disableForm();
+    }
+  }
+
+  // Fungsi untuk mengaktifkan tombol "Daftar"
+  function validateForm() {
+    var submitButton = document.getElementById("submitBtn");
+    submitButton.disabled = false;
+  }
+
+  // Fungsi untuk menonaktifkan tombol "Daftar"
+  function disableForm() {
+    var submitButton = document.getElementById("submitBtn");
+    submitButton.disabled = true;
   }
 
   // Menambahkan event listener untuk memeriksa panjang username saat pengguna memasukkan data
   document.getElementById("username").addEventListener("input", checkUsernameLength);
+
+  // Menambahkan event listener untuk memeriksa panjang password dan kesesuaian kedua password saat pengguna memasukkan data
+  document.getElementById("password").addEventListener("input", checkPassword);
+  document.getElementById("repassword").addEventListener("input", checkPassword);
 </script>
