249c20c4aec61c52e4988dc4e834a0eb83242d35
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-11-09 06:08:22 +0700

    09112023

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://pencatarma.unimar-amni.ac.id/';
+$config['base_url'] = 'https://pmb.unimar-amni.ac.id/';
 // $config['base_url'] = 'http://localhost/pencatarma/';
 
 /*
