a9b1edc4bac2f32f17516cf8671f671694b97858
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-12-06 22:22:46 +0700

    06122023

--- application/views/bau/header.php
+++ application/views/bau/header.php
@@ -62,7 +62,7 @@
         <ul class="nav navbar-nav">
          
           <!-- User Account: style can be found in dropdown.less -->
-          <li class="dropdown notifications-menu open">
+          <li class="dropdown notifications-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
               <i class="fa fa-bell-o"></i>
               <span class="label label-warning">10</span>
