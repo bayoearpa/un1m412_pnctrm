e5c2c77aab0e9b30a5502d2068a21aeef8fa4077
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-12-10 16:53:29 +0700

    10122023

--- application/views/camahatar/seleksigdr2.php
+++ application/views/camahatar/seleksigdr2.php
@@ -17,7 +17,15 @@
                 </div>
             <?php }else if ($validasi == null) { 
               # code... ?>
-               <form action="<?php echo base_url() ?>proses_bukti_bayar" name="form1" id="form1" method="post" enctype="multipart/form-data">               
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-warning"></i> Maaf!</h4>
+                    Silakan melakukan pembayaran terlebih dahulu untuk dapat mengakses halaman ini. terima kasih.
+                </div> 
+
+           <?php }else{ ?>
+                
+                 <form action="<?php echo base_url() ?>proses_bukti_bayar" name="form1" id="form1" method="post" enctype="multipart/form-data">               
                <div class="form-group">
                         <label for="editufsignon">Upload Ijasah D3 (Upload dalam format .pdf)</label>
                         <input type="file" class="form-control" id="upload_ijd3" name="upload_ijd3">
@@ -55,10 +63,6 @@
                 <div class="form-group"><button type="submit" class="btn btn-primary">Simpan</button></div>
 
               </form>
-
-           <?php }else{ ?>
-                
-              
 
             <?php } ?>
             </div>
