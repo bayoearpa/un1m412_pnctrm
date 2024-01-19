b89f88cf39c70985efa361f763267eea6bf4738a
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-01-19 18:38:44 +0700

    19012024

--- application/views/home.php
+++ application/views/home.php
@@ -269,7 +269,7 @@
             </a>
           </div>
            <div class="col-md-6 col-lg-4">
-            <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-10">
+            <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-11">
               <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
                 <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
                   <i class="fa fa-search-plus fa-3x"></i>
@@ -1222,7 +1222,7 @@
     </div>
 
         <!-- Portfolio Modal 4 -->
-    <div class="portfolio-modal mfp-hide" id="portfolio-modal-9">
+    <div class="portfolio-modal mfp-hide" id="portfolio-modal-11">
       <div class="portfolio-modal-dialog bg-white">
         <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
           <i class="fa fa-3x fa-times"></i>
