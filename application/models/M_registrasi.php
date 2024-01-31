a3d1867c59b5aa076b470ff623fd0eeb601cf1dc
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-01-31 11:39:46 +0700

    31012024

--- application/models/M_registrasi.php
+++ application/models/M_registrasi.php
@@ -561,7 +561,7 @@ class m_registrasi extends CI_Model
 
 	     $this->db->from('tbl_seleksi_2024');
 	     $this->db->join('tbl_catar_2024','tbl_seleksi_2024.no = tbl_catar_2024.no','left');
-	     $this->db->join('tbl_catar_2024','tbl_catar_2024.no = tbl_seleksi_samapta_2024.no','left');
+	     $this->db->join('tbl_seleksi_samapta_2024','tbl_catar_2024.no = tbl_seleksi_samapta_2024.no','left');
         $this->db->where('tbl_seleksi_2024.no', $id);
         $query = $this->db->get();
 
