b5e4651696bf46f158519ecd03f22e08051f262e
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-03-25 10:05:52 +0700

    25032024

--- application/views/referral/header.php
+++ application/views/referral/header.php
@@ -65,7 +65,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="<?php echo base_url() ?>assets/front1/img/amni-png.png" class="user-image" alt="User Image">
-              <span class="hidden-xs"><?php echo $this->session->userdata('nama'); ?></span>
+              <span class="hidden-xs"><?php echo $this->session->userdata('name'); ?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
