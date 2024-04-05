04ca7019d28b470c2ecefb20c08174159ef6f1c9
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-04-05 16:48:08 +0700

    05042024

--- application/models/M_registrasi.php
+++ application/models/M_registrasi.php
@@ -103,10 +103,10 @@ class m_registrasi extends CI_Model
 	///////////////////////// cek notifikasi seleksi/////////////////////////////////////////////////
 
     public function getNotifikasiseleksi() {
-        $this->db->select('c.no as nomor, c.nama, c.prodi');
-        $this->db->from('tbl_catar_2024 c');
-        $this->db->join('tbl_seleksi_20242 v', 'c.no = v.no', 'right');
-        $this->db->where('v.cek = belum');
+        $this->db->select('v.no as nomor, v.nama, v.prodi');
+        $this->db->from('tbl_seleksi_20242 c');
+        $this->db->join('tbl_catar_2024 v', 'c.no = v.no', 'left');
+        $this->db->where('c.cek = belum');
         //$this->db->where('v.no IS NULL');
         $this->db->order_by('c.no', 'desc');
         $query = $this->db->get();
