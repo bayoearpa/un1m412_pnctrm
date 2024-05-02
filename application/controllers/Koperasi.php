2e31431645f6472ffa15d80808979d781952445b
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-05-02 13:39:39 +0700

    02052024

--- application/controllers/Koperasi.php
+++ application/controllers/Koperasi.php
@@ -68,7 +68,7 @@ class Koperasi extends CI_Controller {
         );
         $gelombang=$this->m_registrasi->get_data_gelombang($where2);
         $where = array(
-			'tbl_catar_2024.jalur' => 'reguler',			       
+			'tbl_catar_2024.jalur' => array('reguler', 'gdr1'),			       
         );
 		$data['catar'] = $this->m_registrasi->get_data_sudah_daful($where)->result();
 		foreach ($data['catar'] as $row)
