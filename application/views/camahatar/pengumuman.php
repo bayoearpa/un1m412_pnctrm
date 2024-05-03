57848d429fcb3f48e7d7feaca672183541e47896
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-05-03 16:33:48 +0700

    03052024

--- application/views/camahatar/pengumuman.php
+++ application/views/camahatar/pengumuman.php
@@ -27,7 +27,8 @@
                 <?php  foreach ($hasil_sel as $c) { ?>
                   <?php if ($c->hasil == 'lulus'){ ?>
                     <div class="box-body"><h4>Berdasarkan hasil keputusan Panitia Penerimaan Mahasiswa dan Taruna UNIMAR AMNI Semarang Tahun Akademik 2024/2025, ditetapkan bahwa peserta seleksi Penerimaan Mahatar Baru (PMB) sebagaimana tersebut dalam lampiran <b>dinyatakan lulus </b>sebagai Calon Mahasiswa dan Taruna Universitas Maritim AMNI (UNIMAR AMNI) Semarang Tahun Akademik 2024/2025. untuk selengkapnya dapat kalian download dibawah ini :</h4>
-                   <a href="<?php echo base_url() ?>download_pengumuman_januari?>" target="__blank"><button type="button" class="btn btn-primary">Download Pengumuman Lulus Januari</button></a>
+                   <!-- <a href="<?php //echo base_url() ?>download_pengumuman_januari?>" target="__blank"><button type="button" class="btn btn-primary">Download Pengumuman Lulus Januari</button></a>
+                   <a href="<?php //echo base_url() ?>download_pengumuman_januari?>" target="__blank"><button type="button" class="btn btn-primary">Download Pengumuman Lulus Februari-Maret</button></a> -->
                     </div>
                   <?php }else{ ?>
                    <div class="box-body"><h4>Berdasarkan hasil keputusan Panitia Penerimaan Mahasiswa dan Taruna UNIMAR AMNI Semarang Tahun Akademik 2024/2025, ditetapkan bahwa peserta seleksi Penerimaan Mahatar Baru (PMB) sebagaimana tersebut dalam lampiran <b>dinyatakan tidak lulus </b>sebagai Calon Mahasiswa dan Taruna Universitas Maritim AMNI (UNIMAR AMNI) Semarang Tahun Akademik 2024/2025. untuk selengkapnya dapat kalian download dibawah ini :</h4>
