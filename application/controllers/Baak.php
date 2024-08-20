7f2f39fbce9b7e57af970020a58b24c893ffad3d
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-08-20 15:01:53 +0700

    20082024

--- application/controllers/Baak.php
+++ application/controllers/Baak.php
@@ -2603,7 +2603,7 @@ class Baak extends CI_Controller {
          $where = array(
             'jalur' => 'gdr1'
         );
-        $data['catar'] = $this->m_registrasi->get_data_join_where($where)->result();
+        $data['catar'] = $this->m_registrasi->get_data_join_all_seleksigd_whr($where)->result();
         $this->load->view('baak/header');
         $this->load->view('baak/seleksigd',$data);
         $this->load->view('baak/footer');
