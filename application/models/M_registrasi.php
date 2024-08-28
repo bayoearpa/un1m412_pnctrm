cb5ef9a08e7bc30b600f21f75295810225db7c5a
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-08-28 11:52:58 +0700

    28082024

--- application/models/M_registrasi.php
+++ application/models/M_registrasi.php
@@ -967,7 +967,7 @@ class m_registrasi extends CI_Model
      public function get_ref_data_keu() {
         // Select the required fields from tbl_ref
            // Select the necessary fields from tbl_ref
-    $this->db->select('tbl_ref.ref, tbl_ref.nama, tbl_ref.alamat, tbl_ref.no_telp, COUNT(tbl_catar_2024.no) as total');
+    $this->db->select('tbl_ref.ref as referal, tbl_ref.nama as nama_pereferal, tbl_ref.alamat as alamat_pereferal, tbl_ref.no_telp as no_telp_pereferal, COUNT(tbl_catar_2024.no) as total');
     
     // Join with tbl_catar_2024
     $this->db->from('tbl_ref');
