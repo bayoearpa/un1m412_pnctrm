7633f086698afde59aa5ac2335a3ab3b4785269a
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-11-13 12:21:23 +0700

    13112024

--- application/views/camahatar/seleksigdr2_js.php
+++ application/views/camahatar/seleksigdr2_js.php
@@ -6,7 +6,7 @@
             e.preventDefault();
             var filename = $(this).data('filename');
             // Gantilah '/uploads/' dengan direktori tempat Anda menyimpan file
-            var fileUrl = '/assets/upload/2024/upload_ijasah_d3/' + filename;
+            var fileUrl = '/assets/upload/2025/upload_ijasah_d3/' + filename;
             
             // Buka tautan ke file di jendela baru
             window.open(fileUrl, '_blank');
@@ -16,7 +16,7 @@
             e.preventDefault();
             var filename = $(this).data('filename');
             // Gantilah '/uploads/' dengan direktori tempat Anda menyimpan file
-            var fileUrl = '/assets/upload/2024/upload_transkip_d3/' + filename;
+            var fileUrl = '/assets/upload/2025/upload_transkip_d3/' + filename;
             
             // Buka tautan ke file di jendela baru
             window.open(fileUrl, '_blank');
@@ -36,7 +36,7 @@
             e.preventDefault();
             var filename = $(this).data('filename');
             // Gantilah '/uploads/' dengan direktori tempat Anda menyimpan file
-            var fileUrl = '/assets/upload/2024/upload_ktp/' + filename;
+            var fileUrl = '/assets/upload/2025/upload_ktp/' + filename;
             
             // Buka tautan ke file di jendela baru
             window.open(fileUrl, '_blank');
