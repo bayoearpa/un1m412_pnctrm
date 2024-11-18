125ec8409f6a2ffc6a51a10d40c732d18b63e326
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-11-18 10:05:30 +0700

    18112024

--- application/models/M_registrasi.php
+++ application/models/M_registrasi.php
@@ -1039,14 +1039,14 @@ class m_registrasi extends CI_Model
     function get_data_sudah_daful($where)
     {
         // Gantilah 'nama_tabel' dengan nama tabel yang sesuai dalam database Anda
-	     $this->db->select('tbl_catar_2024.nama as nama,
-            tbl_catar_2024.prodi as prodi,
-            tbl_catar_2024.telp as telp,
-            tbl_catar_2024.no,
-            tbl_catar_2024.jk');
+	     $this->db->select('tbl_catar_2025.nama as nama,
+            tbl_catar_2025.prodi as prodi,
+            tbl_catar_2025.telp as telp,
+            tbl_catar_2025.no,
+            tbl_catar_2025.jk');
 
-        $this->db->from('tbl_catar_daful_2024');
-        $this->db->join('tbl_catar_2024', 'tbl_catar_daful_2024.no = tbl_catar_2024.no', 'inner');
+        $this->db->from('tbl_catar_daful_2025');
+        $this->db->join('tbl_catar_2025', 'tbl_catar_daful_2025.no = tbl_catar_2025.no', 'inner');
         $this->db->where($where);
         $query = $this->db->get();
 
