742487d45a134f7278f76d55395c4d9c46d65194
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-12-04 17:10:55 +0700

    04122024

--- application/views/camahatar/pengumuman_gd.php
+++ application/views/camahatar/pengumuman_gd.php
@@ -25,9 +25,11 @@
 
            <?php }else{ ?>
                 <?php  foreach ($catar as $c) { ?>
-                  <div class="box-body"><h4>Berdasarkan hasil keputusan Panitia Penerimaan Mahasiswa dan Taruna UNIMAR AMNI Semarang Tahun Akademik 2024/2025 Gelombang Dini, ditetapkan bahwa peserta seleksi Penerimaan Mahatar Baru (PMB) sebagaimana tersebut dalam lampiran <b>dinyatakan lulus </b>sebagai Calon Mahasiswa dan Taruna Universitas Maritim AMNI (UNIMAR AMNI) Semarang Tahun Akademik 2024/2025. untuk selengkapnya dapat kalian download dibawah ini :</h4>
-                   <a href="<?php echo base_url() ?>download_pengumuman_gelombangdini?>" target="__blank"><button type="button" class="btn btn-primary">Download Pengumuman Lulus Gelombang Dini</button></a>
-                </div>
+                 <!--  <div class="box-body"><h4>Berdasarkan hasil keputusan Panitia Penerimaan Mahasiswa dan Taruna UNIMAR AMNI Semarang Tahun Akademik 2024/2025 Gelombang Dini, ditetapkan bahwa peserta seleksi Penerimaan Mahatar Baru (PMB) sebagaimana tersebut dalam lampiran <b>dinyatakan lulus </b>sebagai Calon Mahasiswa dan Taruna Universitas Maritim AMNI (UNIMAR AMNI) Semarang Tahun Akademik 2024/2025. untuk selengkapnya dapat kalian download dibawah ini :</h4>
+                   <a href="<?php //echo base_url() ?>download_pengumuman_gelombangdini?>" target="__blank"><button type="button" class="btn btn-primary">Download Pengumuman Lulus Gelombang Dini</button></a>
+                </div> -->
+
+                    <div class="box-body"><h4>Anda belum Test Seleksi</h4>
                     <?php } ?>
             <?php } ?>
             </div>
