5d3b1bc5969c73c25d98c5e95c3cc11ccf011a22
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-12-09 11:56:16 +0700

    09122024

--- application/views/camahatar/ukurpakaian.php
+++ application/views/camahatar/ukurpakaian.php
@@ -243,7 +243,7 @@
 
                 <!-- selain Kemartiman -->
                 <h5><b>Pilih salah satu :</b></h5>
-                <center><img src="<?php echo base_url() ?>assets/download/list_uk_jas_alamamater.jpg" width="100%"></center>
+                <center><img src="<?php echo base_url() ?>assets/download/list_uk_jas_almamater.jpg" width="100%"></center>
                  <div class="form-group">
                   <label for="exampleInputEmail1">Jas Almamater :</label>
                  <select class="form-control" name="jas_almamater" id="jas_almamater" required="">
