bacec635402d9ac2e544278e8f664050a1ca9225
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-12-18 15:20:00 +0700

    18122024

--- application/views/bau/daful_js.php
+++ application/views/bau/daful_js.php
@@ -6,7 +6,7 @@
             e.preventDefault();
             var filename = $(this).data('filename');
             // Gantilah '/uploads/' dengan direktori tempat Anda menyimpan file
-            var fileUrl = '/assets/upload/2024/bukti_bayar_daful/' + filename;
+            var fileUrl = '/assets/upload/2025/bukti_bayar_daful/' + filename;
             
             // Buka tautan ke file di jendela baru
             window.open(fileUrl, '_blank');
