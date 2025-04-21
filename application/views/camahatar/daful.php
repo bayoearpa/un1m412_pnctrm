9613ed38acd33615c9620e309f2201a318e79c95
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2025-04-21 11:28:26 +0700

    21042025

--- application/views/camahatar/daful.php
+++ application/views/camahatar/daful.php
@@ -53,6 +53,30 @@
                 </tr>
               </table>
               <hr>
+              <?php if ($prodi == '1' || $prodi == '4'|| $prodi == '5'|| $prodi == '6'|| $prodi == '7'|| $prodi == '8'|| $prodi == '9'|| $prodi == '10') { ?>
+
+                  <!-- /////// prodi teknik nautik -->
+                   <!-- <div class="box-body"><h3>Pelaksanaan Seleksi Offline Untuk Calon Taruna Nautika dan Teknika dapat dilihat selengkapnya dengan.</b> download di bawah ini :</h3>
+                   <a href="<?php //echo base_url() ?>download_pengumuman_seleksi?>" target="__blank"><button type="button" class="btn btn-primary">Pengumuman Seleksi</button></a> -->
+
+                    <div class="box-body"><h3>Silakan download file surat keterangan yang ada dibawah ini, lalu dibawa pada saat <b>Test gelombang dini</b>.file bisa di download di bawah ini :</h3>
+
+                   <label>Surat Keterangan Sehat : </label><br>
+                   <a href="<?php echo base_url() ?>download_suket_sehat_gelombang_dini25?>" target="__blank"><button type="button" class="btn btn-primary">Download Surat Keterangan Sehat</button></a><br>
+                   <label>Surat Keterangan Sanggup Menaati Peraturan :</label><br>
+                   <a href="<?php echo base_url() ?>download_suket_sanggup_menaati_peraturan25?>" target="__blank"><button type="button" class="btn btn-primary">Download Surat Keterangan Sanggup Menaati Peraturan</button></a><br>
+                   <label>Surat Pernyataan Sanggup Tidak Menikah :</label><br>
+                   <a href="<?php echo base_url() ?>download_suket_sanggup_tidak_menikah25?>" target="__blank"><button type="button" class="btn btn-primary">Surat Pernyataan Sanggup Tidak Menikah</button></a><br>
+
+                   <?php if ($jk == 'Wanita') {
+                     # code... ?>
+                    <label>Surat Pernyataan Sanggup Tinggal di Asrama :</label><br>
+                   <a href="<?php echo base_url() ?>download_super_sanggup_tinggal_diasrama25?>" target="__blank"><button type="button" class="btn btn-primary">Download Surat Pernyataan Sanggup Tinggal di Asrama</button></a><br>
+                   <?php }elseif ($prodi == '2' || $prodi == '3') {
+                     # code... ?>
+                    <label>Surat Pernyataan Sanggup Tinggal di Asrama :</label><br>
+                   <a href="<?php echo base_url() ?>download_super_sanggup_tinggal_diasrama25?>" target="__blank"><button type="button" class="btn btn-primary">Download Surat Pernyataan Sanggup Tinggal di Asrama</button></a><br>
+                  <?php }else{} ?>
               <form action="<?php echo base_url() ?>proses_bukti_bayar_daful" name="form1" id="form1" method="post" enctype="multipart/form-data">
               <?php if ($this->session->userdata('jalur')=="gdr1" || $this->session->userdata('jalur')=="reguler"): ?>
                 <!-- <div class="box-body"><h3>Pada Saat Madabintal diharapkan mengisi form yang sudah disiapkan tim PMB.</b> untuk format bisa di download di bawah ini :</h3>
