392c7d9fa1bae5c026e83ce6fbd14997c69502df
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2025-07-07 13:59:49 +0700

    07072025

--- application/views/bau/lihatdatadafulp_js.php
+++ application/views/bau/lihatdatadafulp_js.php
@@ -3,7 +3,7 @@
 		 $('#example1').on('click', '.view-file-button', function() {
             var filename = $(this).data('filename');
             // Gantilah '/uploads/' dengan direktori tempat Anda menyimpan file
-            var fileUrl = '/v1/assets/upload/2025/bukti_bayar_daful/' + filename;
+            var fileUrl = '/assets/upload/2025/bukti_bayar_daful/' + filename;
             
             // Buka tautan ke file di jendela baru
             window.open(fileUrl, '_blank');
