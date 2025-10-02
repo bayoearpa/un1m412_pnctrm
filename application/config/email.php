97b15d0cf9fb1387729dfdf969e8a6da7867c27b
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2025-10-02 10:38:43 +0700

    02102025

--- application/config/email.php
+++ application/config/email.php
@@ -2,7 +2,8 @@
 $config['protocol']  = 'smtp';
 $config['smtp_host'] = 'mail.unimar-amni.ac.id';
 $config['smtp_user'] = 'pmb@unimar-amni.ac.id';
-$config['smtp_pass'] = 'unmr666A11!';
+// $config['smtp_pass'] = 'unmr666A11!';
+$config['smtp_pass'] = 'fywygjFSvJSt39';
 $config['smtp_port'] = 465;
 $config['smtp_crypto'] = 'tls';
 $config['mailtype']  = 'html';
